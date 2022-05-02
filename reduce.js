1f2101cad2a528fefe04198343033177e29b7c27
Dev Workspace <DevWork@Jenna-M-Tuckers-MacBook-2.local>
2022-05-01 19:54:11 -0700

    practice

--- reduce.js
+++ reduce.js
@@ -1,3 +1,5 @@
 const myArr = [2, 5, 7, 21]
-const reduced = myArr.reduce((res, val) => [...res, val], [])
+const reduced = myArr.reduce((res, val) => res.concat(val), '')
+const reduced2 = myArr.reduce((res, val) => [...res, val], [])
 console.log(reduced)
+console.log(reduced2)
