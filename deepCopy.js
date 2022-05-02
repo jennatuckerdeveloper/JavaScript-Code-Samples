d7546fd0249f14ddfc6006a410da799fdedd791a
Dev Workspace <DevWork@Jenna-M-Tuckers-MacBook-2.local>
2022-05-01 19:38:40 -0700

    solution

--- deepCopy.js
+++ deepCopy.js
@@ -1,34 +1,34 @@
 const isPrimitive = (data) => data === null || typeof data !== 'object'
 
-const arrayDeepCopy = (toCopy) => {
+const deepCopy = (toCopy) => {
 	if (isPrimitive(toCopy)) return toCopy
 	if (Array.isArray(toCopy))
-		return toCopy.map((e) => (isPrimitive(e) ? e : arrayDeepCopy(e)))
+		return toCopy.map((e) => (isPrimitive(e) ? e : deepCopy(e)))
 	return Object.assign(
 		{},
 		...Object.keys(toCopy).map((key) =>
 			isPrimitive(toCopy[key])
 				? { [key]: toCopy[key] }
-				: { [key]: arrayDeepCopy(toCopy[key]) }
+				: { [key]: deepCopy(toCopy[key]) }
 		)
 	)
 }
 
 const arr1 = [1, 2, [4, 5, [7, 8, [9, 10]]]]
-const arrCopy = arrayDeepCopy(arr1)
+const arrCopy = deepCopy(arr1)
 arrCopy[2][2][2] = 'NEW!'
 
 console.log(arr1[2][2][2])
 console.log(arrCopy[2][2][2])
 
 let prim = 'myString'
-let primCop = arrayDeepCopy(prim)
+let primCop = deepCopy(prim)
 prim = prim.concat('MORE')
 console.log(prim)
 console.log(primCop)
 
 const obj1 = { a: 1, b: { c: 2, d: 3 } }
-const obj2 = arrayDeepCopy(obj1)
+const obj2 = deepCopy(obj1)
 console.log(obj1)
 console.log(obj2)
 obj1.b.c = '!!!'
