28661745d6e72da1a26322bf1b40c20a1c0aa3f7
Dev Workspace <DevWork@Jenna-M-Tuckers-MacBook-2.local>
2022-05-02 20:54:59 -0700

    7 more solutions

---
+++ codingMeetupSeries/isAgeDiverse.js
@@ -0,0 +1,136 @@
+/*
+You will be given an array of objects (associative arrays in PHP) representing data 
+about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if developers from all of the following age groups have signed up: 
+teens, twenties, thirties, forties, fifties, sixties, seventies, eighties, nineties, 
+centenarian (at least 100 years young).
+false otherwise.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+  { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+  { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+  { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript' }
+];
+your function should return true as there is at least one developer from each age group.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+Age is represented by a number which can be any positive integer up to 199.
+
+*/
+
+const { base } = require('mocha/lib/reporters')
+
+const isAgeDiverse = (devs) => {
+	if (devs.length === 0) return false
+	const ages = devs.map((dev) => dev.age)
+	const bases = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+	const ageInRange = (n, base) => {
+		if (base === 100) return n >= 100
+		return n >= base && n <= base + 9
+	}
+	const ageRangesNotIncluded = bases.reduce((res, base) => {
+		if (!ages.find((age) => ageInRange(age, base))) {
+			res.push(base)
+		}
+		return res
+	}, [])
+	return ageRangesNotIncluded.length === 0
+}
+
+var list1 = [
+	{
+		firstName: 'Harry',
+		lastName: 'K.',
+		country: 'Brazil',
+		continent: 'Americas',
+		age: 19,
+		language: 'Python'
+	},
+	{
+		firstName: 'Kseniya',
+		lastName: 'T.',
+		country: 'Belarus',
+		continent: 'Europe',
+		age: 29,
+		language: 'JavaScript'
+	},
+	{
+		firstName: 'Jing',
+		lastName: 'X.',
+		country: 'China',
+		continent: 'Asia',
+		age: 39,
+		language: 'Ruby'
+	},
+	{
+		firstName: 'Noa',
+		lastName: 'A.',
+		country: 'Israel',
+		continent: 'Asia',
+		age: 40,
+		language: 'Ruby'
+	},
+	{
+		firstName: 'Andrei',
+		lastName: 'E.',
+		country: 'Romania',
+		continent: 'Europe',
+		age: 59,
+		language: 'C'
+	},
+	{
+		firstName: 'Maria',
+		lastName: 'S.',
+		country: 'Peru',
+		continent: 'Americas',
+		age: 60,
+		language: 'C'
+	},
+	{
+		firstName: 'Lukas',
+		lastName: 'X.',
+		country: 'Croatia',
+		continent: 'Europe',
+		age: 75,
+		language: 'Python'
+	},
+	{
+		firstName: 'Chloe',
+		lastName: 'K.',
+		country: 'Guernsey',
+		continent: 'Europe',
+		age: 88,
+		language: 'Ruby'
+	},
+	{
+		firstName: 'Viktoria',
+		lastName: 'W.',
+		country: 'Bulgaria',
+		continent: 'Europe',
+		age: 98,
+		language: 'PHP'
+	},
+	{
+		firstName: 'Piotr',
+		lastName: 'B.',
+		country: 'Poland',
+		continent: 'Europe',
+		age: 128,
+		language: 'JavaScript'
+	}
+]
+
+console.log(isAgeDiverse(list1))
